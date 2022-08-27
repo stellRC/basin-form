c3c048ddb799c07d3350c8850731dc6c3aee8bab
stellRC <stellaffarce@gmail.com>
2022-08-27 14:53:41 -0700

    test all

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  fetch("https://usebasin.com/f/cc928449f4aa", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        console.log("success");
+      } else {
+        console.log("fail");
+      }
+    })
+    .catch((error) => console.log(error));
+});
+
+
+
+
+// function configureAhoy() {
+//     ahoy.configure({
+//       visitsUrl: "https://usebasin.com/ahoy/visits",
+//       eventsUrl: "https://usebasin.com/ahoy/events",
+//       page: "cc928449f4aa" /* Use your form id here */
+//     });
+//     ahoy.trackView();
+//     ahoy.trackSubmits();
+//   }
+
+
+//   configureAhoy()
