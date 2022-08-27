1ac9bbcada53d68ff9b89c52ad1df2c79cb624b9
stellRC <stellaffarce@gmail.com>
2022-08-27 15:04:38 -0700

    saves origin

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
-  fetch("https://usebasin.com/f/cc928449f4aa", {
+  fetch("https://usebasin.com/f/07226c21739b", {
       method: "POST",
       headers: {
         Accept: "application/json",
